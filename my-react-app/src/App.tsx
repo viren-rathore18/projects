fa2761a2224d77967f56d59d68ef2bb817ef558f
viren-rathore18 <singhvirendar2718@gmail.com>
2025-07-23 06:25:00 +0000

    Add weather-fetcher mini project

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,10 +1,53 @@
+
+import React, { useState } from 'react';
+import './App.css'
+
 function App() {
   return (
-    <div>
-      <h1>Hello, Viren!</h1>
+    <div className='container'>
+      <h1>Hello, Viren Rathore!</h1>
+      <h2>I am a full-stack web developer</h2>
+      <ul>
+        <li>One</li>
+        <li>Two</li>
+        <li>Three</li>
+        <li>Four</li>
+      </ul>
       <p>Your React + TypeScript + Vite project is ready.</p>
+      <strong>World's Best software developer & engineer</strong>
+      <hr />
+
+      <h3>Contact Form</h3>
+      <ContactForm/> 
     </div>
   );
 }
+
+function ContactForm() {
+  const [name, setName] = useState('');
+   
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    alert(`Submitted: ${name}`);
+    setName('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Your Name:
+        <input
+        type='text'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder='Enter your name'
+        />
+      </label>
+      <button type='submit'>Submit</button>
+    </form>
+  )
+}
+
 
 export default App;
--- my-ts-app/src/index.ts
+++ my-ts-app/src/index.ts
@@ -2,4 +2,4 @@ function greet(name: string): string {
   return `Hello, ${name}!`;
 }
 
-console.log(greet("Viren"));
+console.log(greet("Viren rathore"));
