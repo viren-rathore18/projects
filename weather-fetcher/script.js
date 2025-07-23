4213f013f6d2acdd554fa9322fd24c3679e16ccf
viren-rathore18 <singhvirendar2718@gmail.com>
2025-07-23 07:16:45 +0000

    Updated working API

--- weather-fetcher/script.js
+++ weather-fetcher/script.js
@@ -16,7 +16,18 @@ button.addEventListener('click', () => {
 
 //Using promises (First approach);
 
-function getWeatherWithPromises(city) {
-  const apiKey = WEATHER_API_KEY;
-  console.log(apiKey)
+async function getWeatherWithPromises(city) {
+  const apiKey = API_KEY;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`;
+  
+
+  try {
+     const res = await fetch (url);
+     if(!res.ok) throw new Error('City not found');
+
+     const data = await res.json();
+     output.innerText = `Temperature in ${city}: ${data.current.temp_c}°C`
+  } catch (err) {
+    output.innerText = `Error: ${err.message}`;
+  }
 }
